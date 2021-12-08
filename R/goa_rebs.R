cd9870dbdfc304ed63a0c163cc94a123c784f61d
Ben Williams <ben.williams@noaa.gov>
2021-12-07 16:50:03 -0900

    update goa_rebs

--- R/goa_rebs.R
+++ R/goa_rebs.R
@@ -16,25 +16,23 @@ goa_rebs <- function(year, akfin_user, akfin_pwd, afsc_user, afsc_pwd, off_yr = 
   # globals ----
   species = "REYE"
   area = "GOA"
-  afsc_species1 = 30050
-  afsc_species2 = 30051
-  afsc_species3 = 30052
-  norpac_species = 307
-  norpac_species2 = 357
+  afsc_species1 = 30050 # Rougheye and Blackspotted rockfish unidentified, use this code for Longline Survey Data
+  afsc_species2 = 30051 # Rougheye rockfish, Sebastes aleutianus, use for Trawl Survey Data
+  afsc_species3 = 30052 # Blackspotted rockfish, Sebastes melanostictus, use for Trawl Survey Data
+  norpac_species = 307 # Rougheye and Blackspotted rockfish unidentified, use for NORPAC Data
+  norpac_species2 = 357 # Blackspotted rockfish, use for NORPAC Data
 
-    if(!is.null(off_yr)){
+
+  if(!is.null(off_yr)){
       # establish akfin connection
-      akfin = DBI::dbConnect(odbc::odbc(), "akfin",
-                             UID = akfin_user, PWD = akfin_pwd)
+      akfin = DBI::dbConnect(odbc::odbc(), "akfin", UID = akfin_user, PWD = akfin_pwd)
 
-      # catch
       q_fish_catch(year, fishery = "fsh", species = species, area = area, akfin = akfin)
-      q_fish_obs(year, fishery = "fsh", norpac_species = norpac_species, area, akfin)
+      q_fish_obs(year, fishery = "fsh", norpac_species = c(norpac_species, norpac_species2), area, akfin)
 
       DBI::dbDisconnect(akfin)
 
-      afsc = DBI::dbConnect(odbc::odbc(), "afsc",
-                            UID = afsc_user, PWD = afsc_pwd)
+      afsc = DBI::dbConnect(odbc::odbc(), "afsc", UID = afsc_user, PWD = afsc_pwd)
 
       q_ts_biomass(year, area = "goa", afsc_species = c(afsc_species1, afsc_species2, afsc_species3), afsc = afsc)
 
@@ -42,20 +40,16 @@ goa_rebs <- function(year, akfin_user, akfin_pwd, afsc_user, afsc_pwd, off_yr = 
 
     } else{
   # establish akfin connection
-  akfin <- DBI::dbConnect(odbc::odbc(), "akfin",
-                          UID = akfin_user, PWD = akfin_pwd)
-
-  # catch
+  akfin = DBI::dbConnect(odbc::odbc(), "akfin", UID = akfin_user, PWD = akfin_pwd)
   q_fish_catch(year, fishery = "fsh", species = species, area = area, akfin = akfin)
-  q_fish_obs(year, fishery = "fsh", norpac_species = c(norpac_species, norpac_species2),
-                area, akfin)
+  q_fish_obs(year, fishery = "fsh", norpac_species = c(norpac_species, norpac_species2),area, akfin)
   q_fish_age_comp(year, fishery = "fsh", norpac_species = c(norpac_species, norpac_species2),
                      area = area, akfin = akfin)
   q_fish_length_comp(year, fishery = "fsh", norpac_species = c(norpac_species, norpac_species2),
                         area = area, akfin = akfin)
-  q_lls_biomass(year, area = "goa", afsc_species = afsc_species, akfin = akfin)
+  # q_lls_biomass(year, area = "goa", afsc_species = afsc_species, akfin = akfin)
 
-  q_lls_length_comp(year, area = "goa", afsc_species = afsc_species, akfin = akfin)
+  # q_lls_length_comp(year, area = "goa", afsc_species = afsc_species, akfin = akfin)
 
   DBI::dbDisconnect(akfin)
 
