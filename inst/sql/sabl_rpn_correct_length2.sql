c4053623bf54a344265428ef790599241d7cc060
Ben <ben.williams@noaa.gov>
2022-08-12 18:53:41 -0800

    add sable

--- inst/sql/sabl_rpn_correct_length2.sql
+++ inst/sql/sabl_rpn_correct_length2.sql
@@ -3,8 +3,6 @@ DISTINCT council_sablefish_management_area,
           council_management_area,
           fmp_management_area,
           geographic_area_name,
-          exploitable,
-          council_management_area
+          exploitable
 FROM  afsc.lls_area_view
 WHERE exploitable = 1
-AND sex IN (1, 2)
--- inst/sql/sabl_rpn_dep.sql
+++ inst/sql/sabl_rpn_dep.sql
@@ -1,3 +1,4 @@
 SELECT
   *
 FROM afsc.lls_ak_wide_3_to_7_depred
+ORDER BY  year ASC
